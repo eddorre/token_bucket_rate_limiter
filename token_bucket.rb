f74ec2eb44b651b8bf1594bd44bb335e6cf10c14
Carlos Rodriguez <carlos@eddorre.com>
2025-02-03 16:06:42 -0800

    Initial commit

---
+++ token_bucket.rb
@@ -0,0 +1,114 @@
+require '~/.ruby_tools/simple_spec.rb'
+require 'debug'
+include Eddorre::SimpleSpec
+
+class TokenBucket
+  REFILL_RATE_PER_SECOND = 5
+
+  attr_reader :identifier, :capacity, :refill_rate, :started, :tokens
+
+  def initialize(identifier:, capacity:, refill_rate: nil)
+    raise ArgumentError unless capacity.positive?
+
+    @identifier = identifier
+    @refill_rate = refill_rate || REFILL_RATE_PER_SECOND
+    @capacity = capacity
+    @started = false
+    @tokens = @capacity
+    @thread = nil
+    @start_time = nil
+  end
+
+  def start
+    @started = true
+    @start_time = Time.now
+
+    begin
+      @thread = Thread.new do
+        while @started do
+          sleep(1)
+          if elapsed_time > refill_rate
+            if tokens <= capacity
+              max_tokens = capacity - tokens
+              @tokens += ([(refill_rate * (elapsed_time / refill_rate)), max_tokens].min).floor
+            end
+          end
+        end
+      end
+    rescue StandardError => e
+      puts "AN EXCEPTION OCCURRED #{e}"
+    end
+  end
+
+  def stop
+    @started = false
+    @thread&.kill
+    @thread = nil
+  end
+
+  def allow?
+    return false unless @started
+    return false unless tokens.positive?
+
+    @tokens -= 1
+    true
+  end
+
+  private
+
+  def elapsed_time
+    return 0 unless @start_time
+
+    Time.now - @start_time
+  end
+end
+
+
+bucket = TokenBucket.new(identifier: '192.168.1.1', capacity: 20)
+
+test 'it should have a capacity of 20' do
+  expect(bucket.capacity).to eq(20)
+end
+
+test 'it should not started by default' do
+  expect(bucket.started).not_to be_truthy
+end
+
+test 'it should set the bucket to started' do
+  bucket.start
+  expect(bucket.started).to be_truthy
+end
+
+test 'it should set the bucket to not started' do
+  bucket.stop
+  expect(bucket.started).not_to be_truthy
+end
+
+test 'it should not allow if stopped' do
+  bucket.stop
+  expect(bucket.allow?).not_to be_truthy
+end
+
+test_bucket = TokenBucket.new(identifier: '192.168.1.1', capacity: 20)
+test_bucket.start
+
+counter = 0
+max_run_time = 60 # in seconds
+request = 0
+
+while counter < max_run_time do
+  number_of_requests = rand(30)
+  puts "SENDING #{number_of_requests} REQUESTS"
+  number_of_requests.times do
+    request += 1
+    allowed = test_bucket.allow?
+    if allowed
+      puts "REQUEST #{request} ALLOWED. NUMBER OF TOKENS #{test_bucket.tokens}."
+    else
+      puts "REQUEST #{request} DENIED. NUMBER OF TOKENS #{test_bucket.tokens}."
+    end
+  end
+  
+  time_to_sleep = sleep(rand(4))
+  counter += time_to_sleep
+end
